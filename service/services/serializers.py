f8a8e61eb79e36a7c149b872cd7b46bf9772e4be
Lord Divan <darveter667@gmail.com>
2023-07-04 12:03:27 +0300

    day 2 - 2

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = '__all__'
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
@@ -10,14 +16,20 @@ class SubscriptionSerializer(serializers.ModelSerializer):
                                                 max_length=111)
     client_email = serializers.CharField(source='client.user.email',
                                          max_length=255)
-    plan = serializers.CharField(source='plan.plan_type',
-                                 max_length=10)
+    plan = PlanSerializer()
     service_name = serializers.CharField(source='service.name',
                                          max_length=50)
     service_full_price = serializers.IntegerField(source='service.full_price')
+    service_price_with_discount = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_service_price_with_discount(instance):
+        return round(instance.service.full_price -
+                     instance.service.full_price * (instance.plan.discount_percent * 0.01), 3)
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'plan', 'client_username',
+        fields = ('id', 'plan', 'client_username',
                   'client_company_name', 'client_email',
-                  'service_name', 'service_full_price')
+                  'service_name', 'service_full_price',
+                  'service_price_with_discount')
