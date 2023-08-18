fb6b5e4c12ddb361d001a4f6d19c278a9b340e0b
Lord Divan <darveter667@gmail.com>
2023-08-18 12:48:52 +0300

    little refactoring

--- service/services/views.py
+++ service/services/views.py
@@ -9,10 +9,16 @@ from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all() \
-        .prefetch_related('plan', 'service',
-                          Prefetch(queryset=Client.objects.all().select_related('user')
-                                   .only('user__email', 'user__username', 'company_name'), lookup='client'))
+    queryset = Subscription.objects.all().prefetch_related(
+        "plan",
+        "service",
+        Prefetch(
+            queryset=Client.objects.all()
+            .select_related("user")
+            .only("user__email", "user__username", "company_name"),
+            lookup="client",
+        ),
+    )
     serializer_class = SubscriptionSerializer
 
     def list(self, request, *args, **kwargs):
@@ -24,15 +30,11 @@ class SubscriptionView(ReadOnlyModelViewSet):
         if total_amount_cache:
             total_amount = total_amount_cache
         else:
-            total_amount = queryset.aggregate(total_amount=Sum('full_price_with_discount')) \
-                .get('total_amount')
-            cache.set(settings.TOTAL_AMOUNT_CACHE_NAME,
-                      total_amount,
-                      60 ** 2)
+            total_amount = queryset.aggregate(
+                total_amount=Sum("full_price_with_discount")
+            ).get("total_amount")
+            cache.set(settings.TOTAL_AMOUNT_CACHE_NAME, total_amount, 60**2)
 
-        response_data = {
-            'result': response.data,
-            'total_amount': total_amount
-        }
+        response_data = {"result": response.data, "total_amount": total_amount}
         response.data = response_data
         return response
